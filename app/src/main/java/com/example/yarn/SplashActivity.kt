9296fb76fa7675bee787e197a20591199cba6057
phatoks <47408616+phatoks@users.noreply.github.com>
2019-12-02 09:07:56 -0800

    SignIn and SignUp

---
+++ app/src/main/java/com/example/yarn/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.yarn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.startActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (FirebaseAuth.getInstance().currentUser == null)
+            startActivity<SignIn>()
+        else
+            startActivity<MainActivity>()
+        finish()
+
+    }
+}
